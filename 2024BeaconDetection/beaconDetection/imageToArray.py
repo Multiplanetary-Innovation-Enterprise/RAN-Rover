61dd3fe544bb58dd2116f206b5e44596466e7723
BeenJeeSoots <bdsutton@mtu.edu>
2024-09-05 19:38:04 -0400

    Labview to PDF Conversion
    Added all labviews relevant to the robot's code into PDF's to diagram from without needing to use Labview, not put into "oldLabviewCode"

--- 2024BeaconDetection/beaconDetection/imageToArray.py
+++ 2024BeaconDetection/beaconDetection/imageToArray.py
@@ -1,9 +1,16 @@
 from PIL import Image
 from findBrightestRowColumn import find_brightest_pixel
 
+
 def image_to_pixel_array(image_path):
     # Open the image file
     image = Image.open(image_path)
+
+    # Standardize dimensions
+    if image.size != (480, 270):
+        image = image.resize((480, 270))
+
+
 
     # Convert the image to grayscale
     grayscale_image = image.convert('L')
@@ -17,7 +24,13 @@ def image_to_pixel_array(image_path):
 
 
 # Example usage:
-print("Brightest pixel coordinates:", find_brightest_pixel(pixel_array=image_to_pixel_array("C:\\Users\\doggl/Desktop\\raspibullshit\\beacondetection\\example.png")))
-print("Brightest pixel coordinates:", find_brightest_pixel(pixel_array=image_to_pixel_array("C:\\Users\\doggl/Desktop\\raspibullshit\\beacondetection\\example2.png")))
-print("Brightest pixel coordinates:", find_brightest_pixel(pixel_array=image_to_pixel_array("C:\\Users\\doggl/Desktop\\raspibullshit\\beacondetection\\example3.png")))
+print("Brightest pixel coordinates:", find_brightest_pixel(pixel_array=image_to_pixel_array("C:\\Users\\doggl\\Documents\\GitHub\\RAN2023\\2024BeaconDetection\\pixel.png")))
+#print("Pixel color value: ", find_brightest_pixel(pixel_array=image_to_pixel_array("C:\\Users\\doggl\\Documents\\GitHub\\RAN2023\\2024BeaconDetection\\pixel.png"))[1][1])
+
+
+#print("Brightest pixel coordinates:", find_brightest_pixel(pixel_array=image_to_pixel_array("C:\\Users\\doggl\\Documents\\GitHub\\RAN2023\\2024BeaconDetection\\small.png")))
+
+#print("Brightest pixel coordinates:", find_brightest_pixel(pixel_array=image_to_pixel_array("C:\\Users\\doggl/Desktop\\raspibullshit\\beacondetection\\example.png")))
+#print("Brightest pixel coordinates:", find_brightest_pixel(pixel_array=image_to_pixel_array("C:\\Users\\doggl/Desktop\\raspibullshit\\beacondetection\\example2.png")))
+#print("Brightest pixel coordinates:", find_brightest_pixel(pixel_array=image_to_pixel_array("C:\\Users\\doggl/Desktop\\raspibullshit\\beacondetection\\example3.png")))
 
